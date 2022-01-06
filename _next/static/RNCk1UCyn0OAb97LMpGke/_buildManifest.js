484847c983b3cbb68e1f8fd8b866da1c50bea568
zena-dual <zena-dual@users.noreply.github.com>
2022-01-06 10:50:13 +0000

    deploy: a819e1a702f40c64c80103917d955e8eb1534640

---
+++ _next/static/RNCk1UCyn0OAb97LMpGke/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3e8899571dc3ea70.css","static/chunks/pages/index-f6a00ba2b46ca16d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
