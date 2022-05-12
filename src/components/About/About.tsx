f486f4df2031b9ec4dacc14aee791e399ce753dc
zena <daikidigdag@gmail.com>
2022-05-12 22:36:56 +0900

    refactor: 作者の名前を削除

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,18 +2,6 @@ import styles from './styles.module.css';
 
 export const About = () => (
   <div className={styles.about}>
-    <span>
-      Created by&nbsp;
-      <a
-        className={styles['creator-twitter']}
-        href="https://twitter.com/zena_dual"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        @zena_dual
-      </a>
-    </span>
-
     <div className={styles['tweet-button-container']}>
       <a className="twitter-share-button" data-size="large" href="https://twitter.com/intent/tweet">
         Tweet
